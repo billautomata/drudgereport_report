c2e2c69ecab979a6e028e9ca3c17a933d6629d91
billautomata <bill@evident.io>
2016-05-09 09:24:33 -0700

    v0

---
+++ index.js
@@ -0,0 +1,14 @@
+var Nightmare = require('nightmare')
+var filename = new Date().valueOf().toString() + '.html'
+var win = new Nightmare()
+    .viewport(1024, 768)
+    .useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
+    .on('did-finish-load', function(err,d){
+      console.log(err,d)
+    })
+    .goto('http://drudgereport.com')
+    .end()
+    .html('./output/'+filename,'HTMLOnly')
+    .then(function(a,b){
+        console.log(a,b,'done')
+    })
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+nvm use 4
+node index.js 
