ebd561fd77880a0c3c75359ab5d21388bce65a91
billautomata <bill@evident.io>
2016-05-10 08:31:14 -0700

    progress

--- find_links.js
+++ find_links.js
@@ -1,6 +1,7 @@
 module.exports = function find_links(line_array){
   var a = []
-  line_array.forEach(function(line){
+  line_array.forEach(function(line, line_idx){
+    // find links where the line contains the link entirely
     if(line.includes('<A HREF=') && line.includes('</A>')){
       var link_begin = line.indexOf('<A HREF=\"')
       var link_end = line.indexOf('\">', link_begin)
@@ -8,6 +9,18 @@ module.exports = function find_links(line_array){
       // console.log(line)
       console.log('link', '\t', line.slice(link_begin + 9, link_end))
       console.log('text', '\t', line.slice(link_end + 2, link_close))
+    }
+    if(line.includes('<A HREF=') && !line.includes('</A>')){
+      // console.log('found a link with a linebreak')
+      var try_line = line + line_array[line_idx+1]
+      if(try_line.includes('<A HREF=') && try_line.includes('</A>')){
+        var link_begin = try_line.indexOf('<A HREF=\"')
+        var link_end = try_line.indexOf('\">', link_begin)
+        var link_close = try_line.indexOf('</A>', link_end)
+        // console.log(line)
+        console.log('link', '\t', try_line.slice(link_begin + 9, link_end))
+        console.log('text', '\t', try_line.slice(link_end + 2, link_close))
+      }
     }
   })
 }
--- index.js
+++ index.js
@@ -36,8 +36,9 @@ html.forEach(function(line,line_idx){
   }
 })
 
-console.log(top_left_index_begin, top_left_index_end)
-console.log(html.slice(top_left_index_begin,top_left_index_end))
+// console.log(top_left_index_begin, top_left_index_end)
+// console.log(html.slice(top_left_index_begin,top_left_index_end))
+console.log('top left')
 find_links(html.slice(top_left_index_begin,top_left_index_end))
 
 // find headline
@@ -49,5 +50,57 @@ html.forEach(function(line, line_idx){
   }
 })
 
+console.log('headline')
 find_links(html.slice(headline_begin, headline_end))
 find_fontsize(html.slice(headline_begin, headline_end))
+
+// left column
+var left_column_index_begin = -1
+var left_column_index_end = -1
+
+html.forEach(function(line, line_idx){
+  if(line.includes('FIRST COLUMN STARTS HERE')){
+    left_column_index_begin = line_idx
+  }
+  if(line.includes('L I N K S    F I R S T    C O L U M N')){
+    left_column_index_end = line_idx
+  }
+})
+
+console.log('left column links')
+find_links(html.slice(left_column_index_begin,left_column_index_end))
+
+// second column
+var second_column_index_begin = -1
+var second_column_index_end = -1
+
+html.forEach(function(line, line_idx){
+  if(line.includes('SECOND COLUMN BEGINS HERE')){
+    second_column_index_begin = line_idx
+  }
+  if(line.includes('L I N K S      S E C O N D     C O L U M N')){
+    second_column_index_end = line_idx
+  }
+})
+
+console.log('second column links')
+console.log(second_column_index_begin, second_column_index_end)
+find_links(html.slice(second_column_index_begin,second_column_index_end))
+
+// third column
+// second column
+var third_column_index_begin = -1
+var third_column_index_end = -1
+
+html.forEach(function(line, line_idx){
+  if(line.includes('THIRD COLUMN STARTS HERE')){
+    third_column_index_begin = line_idx
+  }
+  if(line.includes('L I N K S    A N D   S E A R C H E S     3 R D    C O L U M N')){
+    third_column_index_end = line_idx
+  }
+})
+
+console.log('third column links')
+console.log(third_column_index_begin, third_column_index_end)
+find_links(html.slice(third_column_index_begin,third_column_index_end))
